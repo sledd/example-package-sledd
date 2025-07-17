f8e993d8f0c1f87e5ca5f12b0f636c8446bbcbc7
sledd <65568754+sledd@users.noreply.github.com>
2025-07-17 14:07:50 +0000

    Add initial project structure with rescale functionality and tests

---
+++ tests/test_rescale.py
@@ -0,0 +1,8 @@
+import numpy as np
+from example_package_sledd.rescale import rescale
+ 
+def test_rescale():
+    np.testing.assert_allclose( 
+    rescale(np.linspace(0, 100, 5)), 
+    np.array([0.0, 0.25, 0.5, 0.75, 1.0]), 
+)
